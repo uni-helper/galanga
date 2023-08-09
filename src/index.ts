c466e6e5ab0eaee087b6454a67be53ff0156c2e1
江程训 <dfjsiou@qq.com>
2023-08-09 17:32:18 +0800

    Version_0.2.3

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ export const info = {
 //引入并导出所有原有的子模块
 export { checkNull, checkNotNull, strLength, formatBytes, checkPassword, checkEmail, encode62, decode62, getFileNameFromURL, getFileExtFromString, spliceSiteTitle } from 'galanga';
 export { updateObjectFromImport } from 'galanga';
-export { filterUniqueByProperty } from 'galanga';
+export { filterUniqueByProperty, arrayFilterUniqueItem } from 'galanga';
 export { formatNumber, formatPercent } from 'galanga';
 export { afterTime } from 'galanga';
 export { sleep } from 'galanga';
--- types/index.d.ts
+++ types/index.d.ts
@@ -5,7 +5,7 @@ export declare const info: {
 };
 export { checkNull, checkNotNull, strLength, formatBytes, checkPassword, checkEmail, encode62, decode62, getFileNameFromURL, getFileExtFromString, spliceSiteTitle } from 'galanga';
 export { updateObjectFromImport } from 'galanga';
-export { filterUniqueByProperty } from 'galanga';
+export { filterUniqueByProperty, arrayFilterUniqueItem } from 'galanga';
 export { formatNumber, formatPercent } from 'galanga';
 export { afterTime } from 'galanga';
 export { sleep } from 'galanga';
