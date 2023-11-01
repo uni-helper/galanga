fab47594ec8c1910de29c453f986dbb8e8f5b6e3
江程训 <dfjsiou@qq.com>
2023-11-01 11:28:35 +0800

    暂存更改

--- src/cookie.ts
+++ src/cookie.ts
@@ -1,44 +1,14 @@
-import * as origin from 'galanga';
+import { localCookie as localCookieO } from 'galanga';
 
 export const localCookie = {
-  getItem:(sKey:string) => {
-    let result = null;
-    // #ifdef H5
-    result = origin.localCookie.getItem(sKey);
-    // #endif
-    return result;
-  },
-  setItem:(sKey:string, sValue:string, vEnd?:number | string | Date, sPath?:string, sDomain?:string, bSecure?:boolean) => {
-    let result = false;
-    // #ifdef H5
-    result= origin.localCookie.setItem(sKey, sValue, vEnd, sPath, sDomain, bSecure);
-    // #endif
-    return result;
-  },
-  removeItem:(sKey:string, sPath?:string, sDomain?:string) => {
-    let result = false;
-    // #ifdef H5
-    result = origin.localCookie.removeItem(sKey, sPath, sDomain);
-    // #endif
-    return result;
-  },
-  hasItem:(sKey:string) => {
-    let result = false;
-    // #ifdef H5
-    result = origin.localCookie.hasItem(sKey);
-    // #endif
-    return result;
-  },
-  keys:() => {
-    let result = [];
-    // #ifdef H5
-    result = origin.localCookie.keys();
-    // #endif
-    return result;
-  },
-  clear:() => {
-    // #ifdef H5
-    origin.localCookie.clear();
-    // #endif
-  }
+  //#ifdef H5
+  getItem: localCookieO.getItem,
+  setItem: localCookieO.setItem,
+  removeItem: localCookieO.removeItem,
+  keys: localCookieO.keys,
+  clear: localCookieO.clear,
+  //#endif
+  //#ifndef H5
+  [Symbol.iterator]: () => false,
+  //#endif
 }
